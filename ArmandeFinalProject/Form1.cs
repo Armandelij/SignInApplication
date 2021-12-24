124b2093858bbb9b52358c6e83a7ae4e1cd5d46e
Elijah Armande <armandelij@gmail.com>
2021-12-24 18:04:07 -0500

    First commit

---
+++ ArmandeFinalProject/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ArmandeFinalProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Database connection
+        SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Owner\source\repos\ArmandeFinalProject\ArmandeFinalProject\Mydatabase.mdf;Integrated Security=True;Connect Timeout=30");
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        // Log in button
+        private void button_login_Click(object sender, EventArgs e)
+        {
+            String username, user_password;
+
+            username = txt_username.Text;
+            user_password = txt_password.Text;
+
+            try
+            {
+                String querry = "SELECT * FROM Table WHERE username = '" + txt_username.Text + "' AND password = '" + txt_password.Text +"'";
+                SqlDataAdapter sda = new SqlDataAdapter(querry, conn);
+
+                DataTable dtable = new DataTable();
+                sda.Fill(dtable);
+
+                // if else statements 
+                if(dtable.Rows.Count > 0)
+                {
+                    username = txt_username.Text;
+                    user_password = txt_password.Text;
+
+                    //next loaded page
+                    Menuform form2 = new Menuform();
+                    form2.Show();
+                    this.Hide();
+                }
+
+                else
+                {
+                    MessageBox.Show("Invalid login credentials", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txt_username.Clear();
+                    txt_password.Clear();
+
+                    txt_password.Focus();
+                }
+
+
+
+            }
+            catch
+            {
+                MessageBox.Show("Error, Invalid Log in Credentials");
+            }
+            finally
+            {
+               conn.Close();
+            }
+        }
+
+        // Voids all of the content within the text box
+        private void button_clear_Click(object sender, EventArgs e)
+        {
+            txt_username.Clear();
+            txt_password.Clear();
+
+            txt_username.Focus();
+        }
+
+        // Exit button: Exits out of the form
+        private void button_exit_Click(object sender, EventArgs e)
+        {
+            DialogResult res;
+            res = MessageBox.Show("Do you want to exit", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (res == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else
+            {
+                this.Show();
+            }
+        }
+    }
+}
